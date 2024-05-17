6fca023d294904583c1b1ddef385cf64ec11e602
RaymondShao777 <44079695+RaymondShao777@users.noreply.github.com>
2024-05-17 16:43:48 -0700

    Added basic layout of login page.

---
+++ src/login.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import studySpotLogo from './assets/study-spotter.svg'
+import './login.css'
+
+function Login() {
+	return (
+		<>
+			<div>
+				<img src={studySpotLogo} className="logo" alt="Study Spotter Logo"/>
+			</div>
+			<div>
+				<form>
+					<label> Username: </label><br/>			
+					<input name="Username" /><br/>
+					<label> Password: </label><br/>			
+					<input name="Password" /><br />
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		</>
+	)
+}
+
+export default Login
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login />
+  </React.StrictMode>,
+)
