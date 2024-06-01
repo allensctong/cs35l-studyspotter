08054ec123080786a21a371c0cabf746bc6f7e1b
RaymondShao777 <44079695+RaymondShao777@users.noreply.github.com>
2024-05-31 21:34:59 -0700

    Decided on schemas for http requests

--- schemas/schemas.go
+++ schemas/schemas.go
@@ -1,6 +1,43 @@
 package schemas
 
-type User struct {
+//POST/GET FOR SIGNUP/LOGIN
+type Login struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+//POST FROM UPLOAD
+type Upload struct {
+	Username
+	Image
+	Caption
+}
+
+//POST FROM UPLOAD
+type UploadPfp struct {
+	Username
+	ProfilePicture
+}
+
+//GET FROM MAIN/USER PAGE
+type Post struct {
+	Username string ‘json:”username”’
+	Image 
+	Caption string ‘json:”caption”’
+	Comments # a dictionary or a similar structure with usernames as keys and their corresponding comments as values 
+	LikeCount int ‘json:”like_count”’
+	LikeByUser bool ‘json:”liked_by_user”’
+	LikedUserList list ‘json:”liked_by_user”’
+	UploadTime Time `json:"uploadtime"`
+}
+
+//GET FROM USER PAGE
+type User struct {
+	Username string `json:"username"`
+	IsUsername bool   `json:"isusername"`
+	Bio string `json:"bio"`
+	Following int `json:"following"`
+	Followers int `json:"followers"`
+	ProfilePicture
+	Posts []Post
 }
