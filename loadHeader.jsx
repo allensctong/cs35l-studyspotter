b6320073ec286e9f537be8c53b20693c53db5592
Allen Tong <allensctong@gmail.com>
2024-06-02 13:02:32 -0700

    load profile and add default profile picture

--- loadHeader.jsx
+++ loadHeader.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const ProfileComponent = () => {
+  const [profile, setProfile] = useState({
+    profilePicture: './src/assets/Default_pfp.svg',
+    profileName: '',
+    bio: '',
+    followerCount: 0,
+    followingCount: 0
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        // Fetch the current user
+        const profileResponse = await fetch('http://localhost:8080/api/user/current', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+        });
+
+        if (profileResponse.status !== 200) {
+          console.error('Failed to fetch profile data');
+          return;
+        }
+
+        const profileData = await profileResponse.json();
+        setProfile(profileData);
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  
   return (
      <div className="profile">
       <div className="profile-picture">
-        {/* <img src="profile-picture-url.jpg" alt="Profile" /> */}
+        <img src={profile.profilePicture} alt="Profile" />
       </div>
       <div className="profile-info">
         <h1>Profile Name</h1>
