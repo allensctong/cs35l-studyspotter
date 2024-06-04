8dc69286731ddb319e87c71f65d1d16534917393
c13752hz <104787687+c13752hz@users.noreply.github.com>
2024-06-04 01:20:05 -0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './mainpage';
-import './style_specific_mint.css'; // This can be your global CSS file
-
-ReactDOM.render(<App />, document.getElementById('root'));
