5c12e9dcd4c7df6203ec50b195d6f4e7246e5266
WindskyVG <88114746+WindskyVG@users.noreply.github.com>
2024-06-07 18:20:01 -0700

    Update mpi.jsx

--- mpi.jsx
+++ mpi.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/mainpage.jsx';
+import App from './mainpage.jsx';
 import './main.css'; // This can be your global CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
