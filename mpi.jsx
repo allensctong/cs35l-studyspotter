4d64b1c7820388615261aa89b10f91297925ab12
WindskyVG <88114746+WindskyVG@users.noreply.github.com>
2024-06-07 17:52:41 -0700

    Update mpi.jsx

--- mpi.jsx
+++ mpi.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './mainpage.jsx';
-import './style_specific_mint.css'; // This can be your global CSS file
+import './main.css'; // This can be your global CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
